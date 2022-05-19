dada46dbdb1f912224ab0b93738889898443285a
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-05-19 12:40:02 +0300

    Stripe - Actions and Reducers

--- client/src/components/Checkout.js
+++ client/src/components/Checkout.js
@@ -1,16 +1,18 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import StripeCheckout from "react-stripe-checkout";
-
+import { placeOrder } from '../actions/orderActions'
 import Error from "../components/Error";
 import Loading from "../components/Loading";
 import Success from "../components/Success";
+
 export default function Checkout({ subtotal }) {
   const orderstate = useSelector((state) => state.placeOrderReducer);
   const { loading, error, success } = orderstate;
-
+  const dispatch = useDispatch()
   function tokenHandler(token) {
     console.log(token);
+    dispatch(placeOrder(token , subtotal))
   }
 
   return (
