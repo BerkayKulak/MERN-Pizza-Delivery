f51d83c0e7cd3cd2220c590480fcdeba566206d8
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-05-19 13:03:08 +0300

    Save Orders in Database

--- routes/ordersRoute.js
+++ routes/ordersRoute.js
@@ -4,6 +4,7 @@ const { v4: uuidv4 } = require("uuid");
 const stripe = require("stripe")(
   "sk_test_51IuZTULjHP9VtxkSjIkrD7E0bdk0F3bsQIecq6KpwkhUtwPFlpd4xx4RqCdDrD8dMbptDJbWyZjPSMEGRbC67S1y00W6ZbmMMz"
 );
+const Order = require("../models/orderModel");
 
 router.post("/placeorder", async (req, res) => {
   const { token, subtotal, currentUser, cartItems } = req.body;
@@ -25,9 +26,26 @@ router.post("/placeorder", async (req, res) => {
       }
     );
     if (payment) {
-      res.send("Payment Done");
+      const neworder = new Order({
+        name: currentUser.name,
+        email: currentUser.email,
+        userid: currentUser._id,
+        orderItems: cartItems,
+        orderAmount: subtotal,
+        shippingAddress: {
+          street: token.card.address_line1,
+          city: token.card.address_city,
+          country: token.card.address_country,
+          pincode: token.card.address_zip,
+        },
+        transactionId: payment.source.id,
+      });
+
+      neworder.save();
+
+      res.send("Order placed successfully");
     } else {
-      res.send("Payment Failed");
+      res.send("Payment failed");
     }
   } catch (error) {
     return res.status(400).json({ message: "Something Went Wrong" });
